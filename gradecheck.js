7a45b3afa7787fab513e14f846fa49fc295a7777
Avery Wagar <ajmw.subs@gmail.com>
2018-04-05 19:15:20 -0700

    Works upto 302 - Document moved:

--- gradecheck.js
+++ gradecheck.js
@@ -1,51 +1,12 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const tough = require('tough-cookie');
 
-// // Return grades + gpa
-// async function check(user, pass){
-//   var grades;
-
-//   var formData = {
-//     'Account': user,
-//     'pw': pass
-//   };
-
-//   await new Promise((r,j) => {
-//     makeRequest(r,j, formData);
-//   }).then((result) => {
-//     grades = parseGrades(result.Request);
-//     console.log(grades);
-//   }).catch((err) => {
-//     console.error(err);
-//   });
-
-// }
-
-// function makeRequest(resolve, reject, formData){
-//   request.post({
-//     url: 'https://ps.seattleschools.org/',
-//     formData: formData
-//   }, (err, res) => {
-//     if (err){
-//       reject(err);
-//     }
-//     else {
-//       resolve(res);
-//     }
-
-//   });
-
-// }
-
-// function parseGrades(grades){
-//   return grades;
-// }
-
-
-
-
+/** Login and download grades
+ * @param {string} username - sps_username for user
+ * @param {string} password - unhashed password for user
+ */
 function check(username, password) {
-  const rp = require('request-promise');
-  const cheerio = require('cheerio');
-  const tough = require('tough-cookie');
 
   // Easy creation of the cookie - see tough-cookie docs for details
   let cookie = new tough.Cookie({
@@ -63,44 +24,81 @@ function check(username, password) {
 
 
   const originOptions = {
-    //method: 'POST',
+    // method: 'POST',
     uri: 'https://ps.seattleschools.org/public/home.html',
     headers: { 'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3180.0 Safari/537.36'},
     transform: function (body) {
       // console.log(body);
       return cheerio.load(body);
     },
-    form: {
-      pw: password,
-      Account: username,
-      submit: 'submit'
-    },
     simple: false, 
-    resolveWithFullResponse: true
+    resolveWithFullResponse: true,
+    jar: cookiejar
   };
 
   rp(originOptions)
     .then(($) => {
-      // console.log($.html());
-      // gradeOptions.pstoken = ($('#pstoken').text());
-      // gradeOptions.contextData = ($('#contextData').text());
-      // gradeOptions.dbpw = ($('#dbpw').text());
-      console.log($('#LoginForm').find('#pw').html());
-      console.log($('body').html());
+      var gradeOptions = {
+        method: 'POST',
+        uri: 'https://ps.seattleschools.org/guardian/home.html',
+        headers: { 'Host': 'ps.seattleschools.org', 'Connection': 'keep-alive', 'Content-Length': 448, 'Cache-Control': 'max-age=0', 'Origin': 'https://ps.seattleschools.org', 'Upgrade-Insecure-Requests': 1, 'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3251.0 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'Referer': 'https://ps.seattleschools.org/public/home.html', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-US,en;q=0.9', },
+        data: {
+          pstoken: '',
+          contextData: '',
+          dbpw: '',
+          translator_username: '',
+          translator_password: '',
+          translator_ldappassword: '',
+          returnUrl: '',
+          serviceName: 'PS Parent Portal',
+          serviceTicket: '',
+          pcasServerUrl: '/',
+          credentialType: 'User Id and Password Credential',
+          pw: '9a135bc413b98ae45e304f991a500649',
+          translatorpw:''
+        },
+        transform: function (body) {
+          return cheerio.load(body);
+        },
+        jar: cookiejar,
+        // simple: false, 
+        resolveWithFullResponse: true
+      };
 
-      // Get grades
-      // rp(gradeOptions)
-      //   .then(($) => {
+      // Assign usernam/password from our app
+      gradeOptions.data.Account = username;
+      gradeOptions.data.ldappassword = password;
 
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
+
+      // SPS "Security mesaures"
+      gradeOptions.data.pstoken = $('body').find('#LoginForm').children().attr('name', 'pstoken').val(); // Power School token!!!
+      gradeOptions.data.contextData = $('body').find('#LoginForm').children().attr('name', 'contextData').val(); // ContextData
+      gradeOptions.data.dbpw = $('body').find('#LoginForm').children().attr('name', 'dbpw').val(); // database password? What?
+      gradeOptions.data.pcasServerUrl = $('body').find('#LoginForm').children().attr('name', 'pcasServerUrl').val(); // Always '/'
+
+      // console.log(gradeOptions.data.pstoken);
+
+      getGrades(gradeOptions, cookiejar);
+
 
     })
     .catch((err) => {
-          console.log(err);
+      console.log(err);
     });
 }
+
+// Get grades
+function getGrades(options, cookiejar) {
+  rp(options).then(($) => {
+    console.log($);
+
+  }).catch((err) => {
+    console.log(err);
+
+  });
+}
+
+
 module.exports = {
   check
 };
