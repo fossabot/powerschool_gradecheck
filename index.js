fef659c2b5e95c7db8f75f1641548efc5eef3148
Nathan <nlaha@outlook.com>
2018-04-07 10:18:34 -0700

    index.js no longer stops after no input
    index.js now no longer stops working when user doesn't input anything for username prompt.

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const readline = require('readline');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 var username, pw;
+prompts();
 
-rl.question('What is your SPS Username?\n', (answer) => {
-  // TODO: Log the answer in a database
-  if (answer != ""){
-    username = answer;
-    cb();
-  }
-  else {
-    console.log('Your really bad at this.');
-  }
-}
-);
-
-function cb(){
-  rl.question('What is your SPS password?\n', (answer) => {
+function prompts () {
+  rl.question('What is your SPS Username?\n', (answer) => {
     // TODO: Log the answer in a database
-    pw = answer;
-    rl.close();
-    run();
-  });
-}
+    if (answer != ""){
+      username = answer;
+      cb();
+    }
+    else {
+      console.error('You are really bad at this :(\n');
+      prompts();
+    }
+  }
+  );
 
-function run(){
-  console.log('Hacking!');
-  require('./gradecheck').check(username, pw);
+  function cb(){
+    rl.question('What is your SPS password?\n', (answer) => {
+      // TODO: Log the answer in a database
+      pw = answer;
+      rl.close();
+      run();
+    });
+  }
+
+  function run(){
+    console.log('Hacking!');
+    require('./gradecheck').check(username, pw);
+  }
 }
