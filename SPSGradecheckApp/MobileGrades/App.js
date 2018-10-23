2457819c2f8aabb7a9b8d8a4d70b4b15c7c06770
Nathan <nlaha@outlook.com>
2018-10-22 20:36:11 -0700

    App first steps. Now is functional

--- SPSGradecheckApp/MobileGrades/App.js
+++ SPSGradecheckApp/MobileGrades/App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Alert, Navigator, NativeModules} from 'react-native';
-import { Button, COLOR, ThemeContext, getTheme } from 'react-native-material-ui';
+import {Platform, StyleSheet, Text, View, Alert, Navigator, NativeModules, ViewPagerAndroid} from 'react-native';
+import { Button, COLOR, ThemeContext, getTheme, Card } from 'react-native-material-ui';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { TextField } from 'react-native-material-textfield';
 import Spinner from 'react-native-loading-spinner-overlay';
@@ -29,8 +29,10 @@ var username = '';
 var password = '';
 var loginpage = '';
 var isLoading = false;
+var SemesterGrades;
 
 type Props = {};
+
 export default class App extends Component<Props> {
   state = {
     spinner: false
@@ -100,8 +102,38 @@ export default class App extends Component<Props> {
       .then((responseHTML1) => {
         const $ = cheerio.load(responseHTML1); 
 
-        var gradestable = $('body').find('#tblgrades')
-        console.log(gradestable)
+        var table = []; 
+        $('body').find('#tblgrades').children().map(function(i, e) {
+              table[i] = $(this).find('.bold').text();
+              try {
+                table[i] = table[i].match(/\d+/g).map(Number);
+
+                var uniqueLetters = [];
+                var nonUniqueLetters = [];
+                table[i].split('').filter(function(letter) {
+                  if(uniqueLetters.indexOf(letter) == -1) {
+                    uniqueLetters.push(letter);
+                  } else {
+                    nonUniqueLetters.push(letter);
+                  }
+                });
+                
+                var result = uniqueLetters.filter(function(ltr) {
+                  if(nonUniqueLetters.indexOf(ltr) == -1) {
+                    return ltr;
+                  }
+                });
+
+                table[i] = result.join('')
+              } catch {
+
+              }
+        })
+        console.log(table[3]);
+
+        SemesterGrades = table.map((grade) => <View key={grade[0]} style={styles.gradeView}><Card><Text style={styles.gradeViewText}>{grade[0]}</Text></Card></View>)
+        SemesterGrades.splice(0 , 3)
+
 
         // Turn off spinner
         this.setState({
@@ -119,33 +151,61 @@ export default class App extends Component<Props> {
   render() {
     return (
       <ThemeContext.Provider value={getTheme(uiTheme)}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Mobile Grades</Text>
-        <View style={styles.contentContainer}>
-          <TextField
-            label='SPS Username'
-            value=''
-            onChangeText={ (us) => {username = us; }}
-          />
-          <TextField
-            label='SPS Password'
-            value=''
-            onChangeText={ (pw) => {password = pw; }}
-          />
-          <Button style={{ container: { height: 40, borderRadius: 85, marginTop: 30 }, text: { fontSize: 20 } }} raised primary text="Login" onPress={this.login.bind(this)} />
-          <Spinner
-          visible={this.state.spinner}
-          textContent={'Logging in...'}
-          textStyle={styles.spinnerTextStyle}
-          />
+      <ViewPagerAndroid
+        style={styles.viewPager}
+        initialPage={0}>
+        <View style={styles.pageStyle} key="1">
+        <View style={styles.container}>
+          <Text style={styles.title}>Mobile Grades</Text>
+          <View style={styles.contentContainer}>
+            <TextField
+              label='SPS Username'
+              value=''
+              onChangeText={ (us) => {username = us; }}
+            />
+            <TextField
+              label='SPS Password'
+              value=''
+              onChangeText={ (pw) => {password = pw; }}
+            />
+            <Button style={{ container: { height: 40, borderRadius: 85, marginTop: 30 }, text: { fontSize: 20 } }} raised primary text="Login" onPress={this.login.bind(this)} />
+            <Spinner
+            visible={this.state.spinner}
+            textContent={'Logging in...'}
+            textStyle={styles.spinnerTextStyle}
+            />
+          </View>
         </View>
-      </View>
+          <Text>Created by Nathan Laha</Text>
+        </View>
+        <View style={styles.pageStyle} key="2">
+          {SemesterGrades}
+          <View>
+            <Text>If data is not up to date, please log in again.</Text>
+          </View>
+        </View>
+      </ViewPagerAndroid>
       </ThemeContext.Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  viewPager: {
+    flex: 1
+  },
+  pageStyle: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  gradeView: {
+    fontSize: 20,
+    padding: 20,
+  },
+  gradeViewText: {
+    fontSize: 20,
+    padding: 20,
+  },
   spinnerTextStyle: {
     color: '#FFF'
   },
